39f7e8f8ea79dc2987fc303b23107f2bbfa2c752
Jordi Sola <jordisola@redhat.com>
2020-09-29 11:29:27 +0200

    Jordisola/expense (#3)
    * Add expense service
    
    * fix(expense): Show correct application info
    
    * fix(expense) Release version

---
+++ expense-restful-service/src/main/java/org/acme/rest/json/ExpenseResource.java
@@ -0,0 +1,49 @@
+package org.acme.rest.json;
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/expenses")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ExpenseResource {
+    @Inject
+    public ExpenseService expenseService;
+
+    @GET
+    public Set<Expense> list() {
+        return expenseService.list();
+    }
+
+    @POST
+    public Expense create(Expense expense) {
+        return expenseService.create(expense);
+    }
+
+    @DELETE
+    @Path("{uuid}")
+    public Set<Expense> delete(@PathParam("uuid") UUID uuid) {
+        if (!expenseService.delete(uuid)) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        return expenseService.list();
+    }
+
+    @PUT
+    public void update(Expense expense) {
+        expenseService.update(expense);
+    }
+}
