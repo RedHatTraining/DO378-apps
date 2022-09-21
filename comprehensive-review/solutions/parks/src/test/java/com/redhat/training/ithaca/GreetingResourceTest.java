2d1dd964e1b147287953c7f3b0c95447a5f4dbae
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-09-21 13:19:53 +0200

    parks: add integration test

--- comprehensive-review/solutions/parks/src/test/java/com/redhat/training/ithaca/GreetingResourceTest.java
+++ comprehensive-review/solutions/parks/src/test/java/com/redhat/training/ithaca/GreetingResourceTest.java
@@ -4,18 +4,21 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.emptyArray;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testParksList() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when()
+                .get( "/parks" )
+                .then()
+                .statusCode( 200 )
+                .and()
+                .body( not( emptyArray() ) );
     }
 
 }
