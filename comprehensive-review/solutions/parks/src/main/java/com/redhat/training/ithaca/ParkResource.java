faa2b61effb08afff73016fc963705b0fa88bedb
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-09-21 14:59:15 +0200

    parks: add swaggerui and operation annotation

--- comprehensive-review/solutions/parks/src/main/java/com/redhat/training/ithaca/ParkResource.java
+++ comprehensive-review/solutions/parks/src/main/java/com/redhat/training/ithaca/ParkResource.java
@@ -5,10 +5,17 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 @Path("/parks")
+
 public class ParkResource {
 
     @GET
+    @Operation(
+        summary = "List parks",
+        description = "List all the parks registered in the system"
+    )
     public List<Park> hello() {
         return Park.listAll();
     }
