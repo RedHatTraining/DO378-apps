097a967055e50a9951c232fcbd3eae50cf63fd29
shatakshi06 <shajain@redhat.com>
2022-09-23 13:15:22 +0530

    Add files to Weather service

---
+++ comprehensive-review/solutions/weather/src/main/java/com/redhat/training/ithaca/Alert.java
@@ -0,0 +1,10 @@
+package com.redhat.training.ithaca;
+
+import javax.persistence.Entity;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Alert extends PanacheEntity {
+    
+    
+}
