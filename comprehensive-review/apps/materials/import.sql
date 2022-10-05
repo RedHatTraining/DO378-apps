cae797e0c574244050404db7bb6793be3f39f9af
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-05 15:43:18 +0200

    add missing materials

---
+++ comprehensive-review/apps/materials/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    park(id, name, city, status, size)
+VALUES
+    (1, 'Vondelpark', 'Amsterdam', 1, 800),
+    (2, 'Parc du Cinquantenaire', 'Brussels', 1, 700),
+    (3, 'Parque Ibirapuera', 'São Paulo', 1, 1000),
+    (4, 'Parc Güell', 'Barcelona', 1, 500);
+
