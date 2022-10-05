80fc31e9ad7f5a81101aff79ad81f8e24b237a1f
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-05 16:08:19 +0200

    remove unused dependency

--- comprehensive-review/solutions/parks/src/test/java/com/redhat/smartcity/ParksResourceTest.java
+++ comprehensive-review/solutions/parks/src/test/java/com/redhat/smartcity/ParksResourceTest.java
@@ -4,7 +4,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.emptyArray;
 
