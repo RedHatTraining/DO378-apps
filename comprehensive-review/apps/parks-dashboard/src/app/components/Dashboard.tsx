978f9d72a30b8bf888112a8cba0cf30c7f3c4753
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-05 15:03:44 +0200

    fix frontend bug

--- comprehensive-review/apps/parks-dashboard/src/app/components/Dashboard.tsx
+++ comprehensive-review/apps/parks-dashboard/src/app/components/Dashboard.tsx
@@ -35,7 +35,7 @@ export function Dashboard(): JSX.Element {
                 setReady(true);
             });
 
-    });
+    }, []);
 
     async function getParks() {
         const parks = await ParksService.all();
