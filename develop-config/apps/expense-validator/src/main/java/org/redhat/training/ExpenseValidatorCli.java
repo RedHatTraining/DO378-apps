75721b0b21342fc98ea30f965a8b060dea0ce87b
Pablo Solar Vilariño <psolarvi@redhat.com>
2022-10-05 15:12:08 +0200

    Added base CLI application

---
+++ develop-config/apps/expense-validator/src/main/java/org/redhat/training/ExpenseValidatorCli.java
@@ -0,0 +1,27 @@
+package org.redhat.training;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+import javax.inject.Inject;
+
+@QuarkusMain
+public class ExpenseValidatorCli implements QuarkusApplication {
+    @Inject
+    ExpenseValidator validator;
+
+    @Override
+    public int run(String... args) throws Exception {
+        int amountValue = Integer.parseInt(args[0]);
+
+        if (validator.isValidAmount(amountValue)) {
+            System.out.println("Valid amount: " + amountValue);
+
+            return 0;
+        }
+
+        System.out.println("Invalid amount: " + amountValue);
+
+        return 1;
+    }
+}
