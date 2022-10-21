8ba8744342a112eeac45d090640bd9d817d16022
Pablo Solar Vilariño <psolarvi@redhat.com>
2022-10-21 13:10:45 +0200

    Added all the imports

--- develop-lab/apps/microservice-speaker/src/main/java/com/redhat/training/speaker/Speaker.java
+++ develop-lab/apps/microservice-speaker/src/main/java/com/redhat/training/speaker/Speaker.java
@@ -1,5 +1,11 @@
 package com.redhat.training.speaker;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 import java.util.UUID;
 
 public class Speaker {
