6971903c8d419b706a0457c9e565d216594d58ca
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-24 17:28:54 +0200

    prepare app

---
+++ test-unit/apps/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
@@ -0,0 +1,13 @@
+package com.redhat.training.expenses;
+
+import org.junit.jupiter.api.Test;
+import com.redhat.training.expenses.Expense.PaymentMethod;
+
+public class ExpenseCreationTest {
+
+    @Test
+    public void testCreateExpense() {
+
+    }
+
+}
