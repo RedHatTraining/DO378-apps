16ba75d589139b628ac8ffce30965c92c173780b
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-24 17:15:06 +0200

    rename integration test

--- test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationIT.java
+++ test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationIT.java
@@ -3,7 +3,7 @@ package com.redhat.training.expenses;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class NativeExpenseResourceIT extends ExpenseCreationTest {
+public class ExpenseCreationIT extends ExpenseCreationTest {
 
     // Execute the same tests but in native mode.
 }
