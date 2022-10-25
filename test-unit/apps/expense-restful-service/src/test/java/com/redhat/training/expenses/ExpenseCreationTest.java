995b0652c8b395c95cb703d500a637c76322ac21
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-25 16:26:42 +0200

    include HTTPTestResource in solution

--- test-unit/apps/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
+++ test-unit/apps/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
@@ -1,5 +1,6 @@
 package com.redhat.training.expenses;
 
+import java.net.URI;
 import org.junit.jupiter.api.Test;
 import com.redhat.training.expenses.Expense.PaymentMethod;
 
--- test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
+++ test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import com.redhat.training.expenses.Expense.PaymentMethod;
 import io.quarkus.test.junit.QuarkusTest;
 
+import java.net.URI;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -13,15 +17,19 @@ import io.restassured.http.ContentType;
 @QuarkusTest
 public class ExpenseCreationTest {
 
+    @TestHTTPResource
+    @TestHTTPEndpoint( ExpenseResource.class )
+    URI endpoint;
+
     @Test
     public void testCreateExpense() {
         given()
                 .body( Expense.of( "Test Expense", PaymentMethod.CASH, "1234" ) )
                 .contentType( ContentType.JSON )
-                .post( "/expenses" );
+                .post( endpoint );
 
         when()
-                .get( "/expenses" )
+                .get( endpoint )
                 .then()
                 .statusCode( 200 )
                 .assertThat()
