5e84f1e92069714a796a621c8ad614aa49c3acdf
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-26 09:25:12 +0200

    improve solution
    add TestHTTPEndpoint to ExpenseCreationTest

--- test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
+++ test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationTest.java
@@ -1,35 +1,28 @@
 package com.redhat.training.expenses;
 
 import org.junit.jupiter.api.Test;
-import com.redhat.training.expenses.Expense.PaymentMethod;
+import io.restassured.http.ContentType;
 import io.quarkus.test.junit.QuarkusTest;
-
-import java.net.URI;
-
 import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.common.http.TestHTTPResource;
+import com.redhat.training.expenses.Expense.PaymentMethod;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
-import io.restassured.http.ContentType;
 
 @QuarkusTest
+@TestHTTPEndpoint( ExpenseResource.class )
 public class ExpenseCreationTest {
-
-    @TestHTTPResource
-    @TestHTTPEndpoint( ExpenseResource.class )
-    URI endpoint;
 
     @Test
     public void testCreateExpense() {
         given()
                 .body( Expense.of( "Test Expense", PaymentMethod.CASH, "1234" ) )
                 .contentType( ContentType.JSON )
-                .post( endpoint );
+                .post();
 
         when()
-                .get( endpoint )
+                .get()
                 .then()
                 .statusCode( 200 )
                 .assertThat()
