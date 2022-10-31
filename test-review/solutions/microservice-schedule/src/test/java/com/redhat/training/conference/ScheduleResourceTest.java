f6be8b586e176fb188f8995bc445394373c05f5b
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-10-31 12:30:07 +0100

    change schedule service to use H2 dev services

--- test-review/solutions/microservice-schedule/src/test/java/com/redhat/training/conference/ScheduleResourceTest.java
+++ test-review/solutions/microservice-schedule/src/test/java/com/redhat/training/conference/ScheduleResourceTest.java
@@ -12,14 +12,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.derby.DerbyDatabaseTestResource;
 
 import io.restassured.http.ContentType;
 
 @QuarkusTest
 @TestHTTPEndpoint(ScheduleResource.class)
-@QuarkusTestResource(DerbyDatabaseTestResource.class)
 public class ScheduleResourceTest {
 
     private static final int GIVEN_ID = 101;
