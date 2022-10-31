8fc7f785e643f9424183b26090bff2107a4223fb
Jaime Ramírez <jaime.ramirez@redhat.com>
2022-10-31 16:44:42 +0100

    Changes to the apps for ch03s07 (#23)
    * change schedule service to use H2 dev services
    
    * fix schedule pom.xml file to be consistent with other GEs
    
    * Changes to speaker microservice:
    
    - h2 dev services
    - id generator
    - mockito
    - panache mock
    
    * simplifying session service - work in progress

--- test-review/solutions/microservice-session/src/main/java/com/redhat/training/conference/speaker/SpeakerRepository.java
+++ test-review/solutions/microservice-session/src/main/java/com/redhat/training/conference/speaker/SpeakerRepository.java
@@ -1,9 +1,9 @@
-package org.acme.conference.session;
+package com.redhat.training.conference.speaker;
 
 import javax.enterprise.context.ApplicationScoped;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class SpeakerRepository implements PanacheRepository<Speaker> {
-  
+
 }
