a5df2d979fe9d3bf3d3a0669d560afe4bbef763e
shatakshi06 <shajain@redhat.com>
2023-01-05 10:25:57 -0500

    lab(ch05s07):Securing Quarkus Microservices

---
+++ secure-review/solutions/quarkus-conference/microservice-session/src/main/java/com/redhat/training/SpeakerFromService.java
@@ -0,0 +1,21 @@
+package com.redhat.training;
+
+import javax.json.bind.annotation.JsonbCreator;
+
+public class SpeakerFromService {
+    String uuid;
+    String nameFirst;
+    String nameLast;
+
+    SpeakerFromService(String uuid, String nameFirst, String nameLast) {
+        this.uuid = uuid;
+        this.nameFirst = nameFirst;
+        this.nameLast = nameLast;
+    }
+
+    @JsonbCreator
+    public static SpeakerFromService of(String uuid, String nameFirst, String nameLast) {
+
+        return new SpeakerFromService(uuid, nameFirst, nameLast);
+    }
+}
---
+++ secure-review/solutions/quarkus-conference/microservice-session/src/test/java/com/redhat/training/NativeSessionResourceIT.java
@@ -0,0 +1,6 @@
+package com.redhat.training;
+
+public class NativeSessionResourceIT extends SessionResourceTest{
+
+    // Execute the same tests but in native mode.
+}
