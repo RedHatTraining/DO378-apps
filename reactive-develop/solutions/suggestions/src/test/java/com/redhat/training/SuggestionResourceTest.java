d9be22b110c401305450dda460f963d12acd5b26
Eduardo Ramirez <eramirez@redhat.com>
2023-01-10 14:11:28 +0100

    Move tests to skeleton and add validation.

--- reactive-develop/solutions/suggestions/src/test/java/com/redhat/training/SuggestionResourceTest.java
+++ reactive-develop/solutions/suggestions/src/test/java/com/redhat/training/SuggestionResourceTest.java
@@ -4,13 +4,17 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 
 @QuarkusTest
 @TestHTTPEndpoint(SuggestionResource.class)
@@ -22,14 +26,20 @@ public class SuggestionResourceTest {
 
     @Test
     public void testCreateEndpoint() {
-        Suggestion returnedSuggestion = createSuggestion( 1L, 103L );
+        Suggestion returnedSuggestion = createSuggestion( 1L, 103L, 200 );
 
         assertThat( returnedSuggestion.id ).isNotNull();
     }
 
     @Test
+    public void testInvalidCreateParameters() {
+        createSuggestion( null, 103L, 400, "clientId", "must not be null" );
+        createSuggestion( 2L, null, 400, "itemId", "must not be null" );
+    }
+
+    @Test
     public void testGetEndpoint() {
-        Suggestion inserted = createSuggestion( 2L, 104L );
+        Suggestion inserted = createSuggestion( 2L, 104L, 200 );
 
         Suggestion retrieved = given()
         .when()
@@ -44,9 +54,9 @@ public class SuggestionResourceTest {
 
     @Test
     public void testListEndpoint() {
-        createSuggestion( 3L, 105L );
-        createSuggestion( 4L, 106L );
-        createSuggestion( 5L, 107L );
+        createSuggestion( 3L, 105L, 200 );
+        createSuggestion( 4L, 106L, 200 );
+        createSuggestion( 5L, 107L, 200 );
 
         List<Suggestion> suggestions = given()
         .when()
@@ -59,16 +69,23 @@ public class SuggestionResourceTest {
         assertThat( suggestions ).size().isEqualTo( 3 );
     }
 
-    private Suggestion createSuggestion( long clientId, long itemId ) {
+    private Suggestion createSuggestion( Long clientId, Long itemId, int expectedStatusCode, String... errors ) {
         Suggestion newSuggestion = new Suggestion( clientId, itemId );
 
-        return given().body( newSuggestion )
+        ValidatableResponse statusCode = given().body( newSuggestion )
             .when()
                 .contentType( ContentType.JSON )
                 .post()
             .then()
-                .statusCode( 200 )
+                .statusCode( expectedStatusCode );
+
+        if( expectedStatusCode == 200 ) {
+            return statusCode
                 .extract()
                 .as( Suggestion.class );
+        } else {
+            Stream.of(errors).map( e -> statusCode.body( Matchers.containsString(e) ) ).toList();
+            return null;
+        }
     }
 }
