99b2bfd896ec5e491e543c79a7d483ebfefd0361
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-01-10 17:24:24 +0100

    working on the solution

---
+++ secure-jwt/solutions/expenses/src/test/java/com/redhat/training/expense/AdminResourceTest.java
@@ -0,0 +1,52 @@
+package com.redhat.training.expense;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class AdminResourceTest {
+
+    @TestHTTPEndpoint(AdminResource.class)
+    @TestHTTPResource("expenses")
+    URL expensesEndpoint;
+
+    @Test
+    public void guestsCannotListExpenses() {
+        given()
+        .when()
+            .get(expensesEndpoint)
+        .then()
+            .statusCode(401);
+    }
+
+    @Test
+    public void adminsCanListAllExpenses() {
+
+        // User "joel" has two expenses
+        var bearerToken = "Bearer " + getJwt("admin");
+
+        given()
+            .headers("Authorization", bearerToken )
+        .when()
+            .get(expensesEndpoint)
+        .then()
+            .statusCode(200)
+            .body("$.size()", is(3));
+    }
+
+
+    private String getJwt(String username) {
+        return given().when()
+            .get("/jwt/" + username)
+        .then()
+            .statusCode(200)
+            .extract().body().asString();
+    }
+}
