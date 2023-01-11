7c7c20182e8a7841cb5141bad86b5953062ebe1d
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-01-11 11:00:22 +0100

    add apps

---
+++ secure-jwt/apps/expenses/src/test/java/com/redhat/training/jwt/JwtGeneratorTest.java
@@ -0,0 +1,47 @@
+package com.redhat.training.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.inject.Inject;
+
+import io.smallrye.jwt.auth.principal.JWTParser;
+import io.smallrye.jwt.auth.principal.ParseException;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class JwtGeneratorTest {
+
+    @Inject
+    JWTParser jwtParser;
+
+    @Test
+    public void userJwtBelongsToUserGroup() throws ParseException {
+        var token = JwtGenerator.generateJwtForRegularUser("testUser");
+
+        JsonWebToken jwt = jwtParser.parse(token);
+
+        assertTrue(jwt.getGroups().contains("USER"), "JWT groups do not contain USER");
+    }
+
+    @Test
+    public void adminJwtBelongsToUserGroup() throws ParseException {
+        var token = JwtGenerator.generateJwtForAdmin("testUser");
+
+        JsonWebToken jwt = jwtParser.parse(token);
+
+        assertTrue(jwt.getGroups().contains("USER"), "JWT groups do not contain USER");
+    }
+
+    @Test
+    public void adminJwtBelongsToAdminGroup() throws ParseException {
+        var token = JwtGenerator.generateJwtForAdmin("testUser");
+
+        JsonWebToken jwt = jwtParser.parse(token);
+
+        assertTrue(jwt.getGroups().contains("ADMIN"), "JWT groups do not contain USER");
+    }
+}
