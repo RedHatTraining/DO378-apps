a0f10ba629d560c7f310f5e42ad41f9e45e48a67
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-01-12 10:19:32 +0100

    tweak apps

--- secure-jwt/apps/expenses/src/main/java/com/redhat/training/jwt/JwtGenerator.java
+++ secure-jwt/apps/expenses/src/main/java/com/redhat/training/jwt/JwtGenerator.java
@@ -10,14 +10,15 @@ public class JwtGenerator {
     private static final String ISSUER = "https://example.com/redhattraining";
 
     public static String generateJwtForRegularUser( String username ) {
-        return Jwt.issuer( ISSUER )
-                .upn( username )
+        return Jwt.upn( username + "@example.com" )
                 .sign();
     }
 
     public static String generateJwtForAdmin( String username ) {
-        return Jwt.issuer( ISSUER )
-                .upn( username )
+        return Jwt.upn( username + "@example.com" )
+                .issuer( ISSUER )
+                .subject( username )
+                .claim( "locale", "en_US" )
                 .sign();
     }
 }
