2373970fa7033c0ce692190b77a3616aeb903cf0
Marek Czernek <mczernek@redhat.com>
2023-01-16 13:36:31 +0100

    Feat(ch05s06): Provide GE applications (#49)

---
+++ secure-sso/apps/expense/get_token.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+if [ $# -lt 2 ]; then
+  echo 1>&2 "Usage: . $0 username password"
+  echo 1>&2 "  available users (username/password):"
+  echo 1>&2 "    user/redhat"
+  echo 1>&2 "    superuser/redhat"
+  exit 1
+fi
+
+SERVER="https://localhost:8888/realms/quarkus/protocol/openid-connect/token"
+SECRET_ID="backend-service"
+SECRET_PW="secret"
+USERNAME="$1"
+PASSWORD="$2"
+
+export TOKEN=$(curl --insecure -s -X POST "$SERVER" \
+ --user ${SECRET_ID}:${SECRET_PW} \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=${USERNAME}" \
+ -d "password=${PASSWORD}" \
+ -d 'grant_type=password' \
+ | jq --raw-output '.access_token'
+ )
+
+if [[ "$TOKEN" == "null" ]] || [[ "$TOKEN" == ""  ]]; then
+    echo 1>&2 "Token was not retrieved! Review input parameters."
+else
+    echo 1>&2 "Token succesfuly retrieved."
+fi
---
+++ secure-sso/apps/expense/src/main/resources/META-INF/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO EXPENSE (UUID,AMOUNT,NAME,PAYMENTMETHOD) 
+VALUES 
+  ('3f1817f2-3dcf-472f-a8b2-77bfe25e79d1', 10,'Kubernetes Patterns', 0),
+  ('4fe78f4f-3335-4585-8677-5d9c8bbb539e', 15,'Red Hat OpenShift for Developers', 0),
+  ('02d13404-1677-45f6-862b-93e5f87e27d3', 15,'Podman in Action', 0);
