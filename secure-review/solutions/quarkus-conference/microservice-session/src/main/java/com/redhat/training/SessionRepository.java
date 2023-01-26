021485094c144817d48f6a69f5d2a6533429d1de
Marek Czernek <mczernek@redhat.com>
2023-01-26 09:40:13 +0100

    Remove old solutions dir

--- secure-review/solutions/quarkus-conference/microservice-session/src/main/java/com/redhat/training/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.training;
-
-import javax.enterprise.context.ApplicationScoped;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-
-@ApplicationScoped
-public class SessionRepository implements PanacheRepository<Session> {
-  
-}
--- secure-review/solutions/quarkus-conference/microservice-session/src/test/java/com/redhat/training/NativeSessionResourceIT.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.training;
-
-public class NativeSessionResourceIT extends SessionResourceTest{
-
-    // Execute the same tests but in native mode.
-}
