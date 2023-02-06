26431dbb0541d940b1be8feb26c71171b1a02170
Pablo Solar Vilariño <60100327+psolarvi@users.noreply.github.com>
2023-02-06 16:52:36 +0100

    fix(*): Updated script to catch all the dependencies (#68)

--- mvn-offline-mode.sh
+++ mvn-offline-mode.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# We fire multiple goals so we track dependencies that are only requested on those goals
+# The ignore of tests is required because some apps will have incomplete tests
+# Spoiler alert: this script might not be perfect!
 find . -name "pom.xml" \
         -not -path "./quarkus-conference/*" \
         -not -path "./quarkus-calculator/*"  \
         -not -path "./quarkus-calculator-monolith/*" \
         -not -path "./istio-tutorial/*" \
-        -execdir /opt/apache-maven/bin/mvn dependency:go-offline \;
+        -execdir /opt/apache-maven/bin/mvn clean dependency:go-offline test -Dmaven.test.failure.ignore=true package \;
