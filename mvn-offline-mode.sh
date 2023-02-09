49385d40fded835d561c93d5238e5986ba14c157
Pablo Solar Vilariño <psolarvi@redhat.com>
2023-02-09 09:51:12 +0100

    fix(*): Added create goal to download the initial BOM

--- mvn-offline-mode.sh
+++ mvn-offline-mode.sh
@@ -3,6 +3,12 @@
 # We fire multiple goals so we track dependencies that are only requested on those goals
 # The ignore of tests is required because some apps will have incomplete tests
 # Spoiler alert: this script might not be perfect!
+cd /tmp && rm -rf tenther && /opt/apache-maven/bin/mvn \
+    com.redhat.quarkus.platform:quarkus-maven-plugin:2.13.5.Final-redhat-00002:create \
+        -DprojectGroupId=com.redhat.training \
+        -DprojectArtifactId=tenther \
+        -DplatformVersion=2.13.5.Final-redhat-00002
+
 find . -name "pom.xml" \
         -not -path "./quarkus-conference/*" \
         -not -path "./quarkus-calculator/*"  \
