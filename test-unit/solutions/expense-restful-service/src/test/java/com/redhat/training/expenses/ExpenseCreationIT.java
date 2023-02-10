cc09a724b4fd5fc93f2b43a8428f41c26893a7ea
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-02-10 17:04:33 +0100

    fix(ch03): pilot feedback

--- test-unit/solutions/expense-restful-service/src/test/java/com/redhat/training/expenses/ExpenseCreationIT.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.training.expenses;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExpenseCreationIT extends ExpenseCreationTest {
-}
