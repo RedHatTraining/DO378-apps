46c027f8c97aaa39263c04a51c6324c2d0011667
Jaime Ramírez <jaime.ramirez@redhat.com>
2023-02-14 15:50:06 +0100

    fix(test-devservices): incorporate ch03s06 pilot feedback (#77)
    * renamed quarkus test annotation
    
    * fix image name to point to internal registry

--- test-devservices/solutions/expense-service/src/test/java/com/redhat/training/rest/WithPostgresDB.java
+++ test-devservices/solutions/expense-service/src/test/java/com/redhat/training/rest/WithPostgresDB.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import io.quarkus.test.common.QuarkusTestResource;
 
-@QuarkusTestResource(value = PostgressDBTestResource.class, restrictToAnnotatedClass = true)
+@QuarkusTestResource(value = PostgresDBTestResource.class, restrictToAnnotatedClass = true)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface WithPostgresDB {
