49a3c0c06f6a4afd60b5becc2fe3f7645fbdc134
Eduardo Ramirez <42619854+ebramirez@users.noreply.github.com>
2023-02-15 15:29:42 +0100

    fix(monitor-trace): Remove prefix from lecture script reference (#81)

--- monitor-trace/solutions/quarkus-calculator/add-tracing.sh
+++ monitor-trace/solutions/quarkus-calculator/add-tracing.sh
@@ -2,17 +2,17 @@
 
 echo "Adding tracing extension to the 'solver' project "
 cd ~/DO378/monitor-trace/solver
-mvn quarkus:add-extension -Dextension="quarkus-smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="smallrye-opentracing"
 cd ..
 
 echo "Adding tracing extension to the 'adder' project "
 cd ~/DO378/monitor-trace/adder
-mvn quarkus:add-extension -Dextension="quarkus-smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="smallrye-opentracing"
 cd ..
 
 echo "Adding tracing extension to the 'multiplier' project "
 cd ~/DO378/monitor-trace/multiplier
-mvn quarkus:add-extension -Dextension="quarkus-smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="smallrye-opentracing"
 cd ..
 
 echo
