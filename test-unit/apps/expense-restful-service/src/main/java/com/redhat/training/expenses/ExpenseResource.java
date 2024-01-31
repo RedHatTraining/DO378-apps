91df2428d9035504652cce481ad67a54710c3e87
Jordi Sola <someth2say@gmail.com>
2024-01-31 13:37:53 +0100

    PTL-10782_PTL-10783_test_unit

--- test-unit/apps/expense-restful-service/src/main/java/com/redhat/training/expenses/ExpenseResource.java
+++ test-unit/apps/expense-restful-service/src/main/java/com/redhat/training/expenses/ExpenseResource.java
@@ -3,20 +3,20 @@ package com.redhat.training.expenses;
 import java.util.List;
 import java.util.UUID;
 
-import javax.inject.Inject;
-import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path( "/expenses" )
 @Consumes( MediaType.APPLICATION_JSON )
