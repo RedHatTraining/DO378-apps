1886e5034fc9c78e7d4c860310241e377b92245c
rafa <rruizher@redhat.com>
2024-03-19 11:56:34 +0100

    Correct also in apps

--- tolerance-policies/apps/monitor/src/test/java/com/redhat/training/NativeInfoResourceIT.java
+++ tolerance-policies/apps/monitor/src/test/java/com/redhat/training/NativeInfoResourceIT.java
@@ -1,7 +1,7 @@
 package com.redhat.training;
 
-import io.quarkus.test.junit5.NativeImageTest
-@NativeImageTest
+
+@QuarkusIntegrationTest
 public class NativeInfoResourceIT extends MonitorResourceTest {
 
     // Execute the same tests but in native mode.
