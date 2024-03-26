7bb16a9ed8733b239aa9bbc1ee73ed33d4187a69
Eduardo Ramirez <42619854+ebramirez@users.noreply.github.com>
2024-03-26 15:57:53 +0100

    PTL-10834 and PTL-10835 monitor_trace (#120)
    * Initial update to RHBQ 3.8
    
    * Update add extension script
    
    * Final updates for RHQB 3.8
    
    * Synchronize otel properties on all services and solutions
    
    ---------
    
    Co-authored-by: Jordi Sola <someth2say@gmail.com>

--- monitor-trace/solutions/quarkus-calculator/add-tracing.sh
+++ monitor-trace/solutions/quarkus-calculator/add-tracing.sh
@@ -2,17 +2,17 @@
 
 echo "Adding tracing extension to the 'solver' project "
 cd ~/DO378/monitor-trace/solver
-mvn quarkus:add-extension -Dextension="smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="opentelemetry"
 cd ..
 
 echo "Adding tracing extension to the 'adder' project "
 cd ~/DO378/monitor-trace/adder
-mvn quarkus:add-extension -Dextension="smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="opentelemetry"
 cd ..
 
 echo "Adding tracing extension to the 'multiplier' project "
 cd ~/DO378/monitor-trace/multiplier
-mvn quarkus:add-extension -Dextension="smallrye-opentracing"
+mvn quarkus:add-extension -Dextension="opentelemetry"
 cd ..
 
 echo
--- monitor-trace/solutions/quarkus-calculator/jaeger.sh
+++ monitor-trace/solutions/quarkus-calculator/jaeger.sh
@@ -2,11 +2,9 @@
 
 echo "Starting the all-in-one Jaeger container "
 podman run --rm --name jaeger \
--p 5775:5775/udp \
--p 6831:6831/udp \
--p 6832:6832/udp \
--p 5778:5778 \
--p 16686:16686 \
--p 14268:14268 \
-registry.ocp4.example.com:8443/redhattraining/do378-jaeger-allinone:1.21.0
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+registry.ocp4.example.com:8443/redhattraining/do378-jaeger-allinone:1.55.0
 
